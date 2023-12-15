cca16db16b6844e61b6ee1af10c5a4c66000ea67
Harry <kidflash7011@gmail.com>
2023-12-15 17:13:07 +0800

    uploading files' logic

---
+++ app.js
@@ -0,0 +1,12 @@
+let queued_images_array = [],
+            saved_form = document.querySelector("#save_form"),
+            queued_form = document.querySelector("#queued_form"),
+            saved_div = document.querySelector(".saved_div"),    
+            queued_div = document.querySelector(".queued_div"),
+            input_div = document.querySelector(".input_div"),
+            server_message = document.querySelector(".server_message"),
+            deleted_images_array = [];
+
+            // Saved in server images
+
+            // Queued in fronted images
